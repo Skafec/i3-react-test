e6d45bb4b3dabbac12ef28ad2769853d58fa68bd
Karlo Skafec <karlo@MacBook-Pro.local>
2021-01-13 15:33:35 +0100

    added all content

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,35 +1,66 @@
-import React from 'react';
-import { fetchPrismicData } from '../../utils/prismicHelpers';
-import { RichText } from 'prismic-reactjs'
+import React from "react";
+import { fetchPrismicData } from "../../utils/prismicHelpers";
+import { RichText } from "prismic-reactjs";
 
 function Home() {
   const [doc, setDocData] = React.useState(null);
 
   React.useEffect(() => {
     // TODO: Check if this React hook attribute passing is implemented correctly
-    fetchPrismicData(setDocData, 'homepage');
+    fetchPrismicData(setDocData, "homepage");
   }, []);
 
   return (
-    <div className="home">
-      {
-        doc ? (
+    <React.Fragment>
+      {doc ? (
+        <div className="home">
           <div className="home__top">
             <div className="home__image">
-              <img src={doc.data.home_image.url} alt={doc.data.home_image.alt} />
+              <img
+                src={doc.data.home_image.url}
+                alt={doc.data.home_image.alt}
+              />
             </div>
             <div className="home__title">
               <h1>{RichText.asText(doc.data.home_title)}</h1>
             </div>
           </div>
-
-
-        ) :
-        <h1>
-          No content
-        </h1>
-      }
-    </div>
+          <div className="home__content">
+            <div className="home__gallery">
+              <div className="gallery__item">
+                <img
+                  src={doc.data.slider_img_1.url}
+                  alt={doc.data.slider_img_1.alt}
+                />
+                <p>
+                  {RichText.asText(doc.data.slider_text_1)}
+                </p>
+              </div>
+              <div className="gallery__item">
+                <img
+                  src={doc.data.slider_img_2.url}
+                  alt={doc.data.slider_img_2.alt}
+                />
+                <p>
+                  {RichText.asText(doc.data.slider_text_2)}
+                </p>
+              </div>
+              <div className="gallery__item">
+                <img
+                  src={doc.data.slider_img_3.url}
+                  alt={doc.data.slider_img_3.alt}
+                />
+                <p>
+                  {RichText.asText(doc.data.slider_text_3)}
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      ) : (
+        <h1>No content</h1>
+      )}
+    </React.Fragment>
   );
 }
 
--- src/components/pages/Story.js
+++ src/components/pages/Story.js
@@ -1,8 +1,69 @@
+import React from "react";
+import { fetchPrismicData } from "../../utils/prismicHelpers";
+import { RichText } from "prismic-reactjs";
+
 function Story() {
+  const [doc, setDocData] = React.useState(null);
+
+  React.useEffect(() => {
+    // TODO: Check if this React hook attribute passing is implemented correctly
+    fetchPrismicData(setDocData, "story_page");
+  }, []);
+
   return (
-    <div className="story">
-      <h1>Story Page</h1>
-    </div>
+    <React.Fragment>
+      { doc ? (
+        <div className="story">
+          <div className="story__left">
+            <h1>
+              { RichText.asText(doc.data.story_title) }
+            </h1>
+          </div>
+          <div className="story__right">
+            <img
+              src={ doc.data.main_image.url }
+              alt={ doc.data.main_image.alt }
+            />
+          </div>
+          <div className="story__left">
+            <p>
+              { RichText.render(doc.data.main_content) }
+            </p>
+            <div className="left__gallery">
+              <div className="gallery__item">
+                <img
+                  src={ doc.data.story_img_1.url }
+                  alt={ doc.data.story_img_1.alt }
+                />
+                <p>
+                  { RichText.render(doc.data.story_text_1) }
+                </p>
+              </div>
+              <div className="gallery__item">
+                <img
+                  src={ doc.data.story_img_2.url }
+                  alt={ doc.data.story_img_2.alt }
+                />
+                <p>
+                  { RichText.render(doc.data.story_text_2) }
+                </p>
+              </div>
+              <div className="gallery__item">
+                <img
+                  src={ doc.data.story_img_3.url }
+                  alt={ doc.data.story_img_3.alt }
+                />
+                <p>
+                  { RichText.render(doc.data.story_text_3) }
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      ) : (
+        <h1>No content</h1>
+      )}
+    </React.Fragment>
   );
 }
 
